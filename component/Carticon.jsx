4fbf1665a878546f58bef51d70bd543b2e2cd7f8
Madhurikorapati <madhurikorapati2002@gmail.com>
2023-02-02 10:37:40 +0530

    initialisation

---
+++ component/Carticon.jsx
@@ -0,0 +1,9 @@
+export default function Carticon(){
+    return(
+        <div className="cart-icon">
+            <div className="icon"></div>
+            <i className="fa-solid fa-cart-shopping"></i>
+            <div className="counter"></div>
+                    </div>
+    );
+}
---
+++ component/Header.jsx
@@ -0,0 +1,14 @@
+import CartIcon from "../../shop/src/Carticon"
+
+export default function Header(){
+   
+        return( 
+        <div className='Header'>
+            <div className='title'>
+            Cart
+            </div>
+            <CartIcon/>
+        </div>
+        );
+}
+  
